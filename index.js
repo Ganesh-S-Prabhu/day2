33afb0ddb8dc290a6009a817a6cb1948eec93fa9
Ganesh-S-Prabhu <93926085+Ganesh-S-Prabhu@users.noreply.github.com>
2022-03-26 15:51:53 +0530

    done

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+const books = require('./data')
+
+app.get("/Home" ,function (request,response){
+    response.send("Hello ");
+});
+
+app.get("/books" ,function (request,response){
+    // response.send("Hello Books");
+    response.send(books);
+});
+
+app.listen(4050,() => {
+
+    console.log("listeining on port 4050");   
+});
+
